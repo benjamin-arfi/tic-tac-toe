73a38743338fb94c7ad0359ea28d1af170ce22f3
benjamin-arfi <you@example.com>
2023-03-21 13:07:05 +0200

    fin projet debut aller plus loin

--- tictactoe.py
+++ tictactoe.py
@@ -1,12 +1,12 @@
 from tkinter import *
 root = Tk()
-root.geometry("500x500")
+root.geometry("400x550")
 root.title("Tic Tac Toe")
 
 frame1 = Frame(root)
 frame1.pack()
-titleLabel = Label(frame1,text="Tic Tac Toe",font=("Arial",25),bg="green")
-titleLabel.pack()
+titleLabel = Label(frame1,text="Tic Tac Toe",font=("Arial",25),bg="green",width=18)
+titleLabel.grid(row=0, column=0, columnspan=3)
 
 frame2 = Frame(root)
 frame2.pack()
@@ -16,6 +16,8 @@ board = {1:"  " , 2:"  " , 3:"  ",
          7:"  " , 8:"  " , 9:"  " }
 
 turn = "x"
+fin_jeu = False
+
 def win(player):
     #gagner en range
     if board[1] == board[2] and board[2] == board[3] and board[3]== player:
@@ -38,13 +40,30 @@ def win(player):
         return True
 
     return False
-def checkgame():
+
+def checkgamenulle():
     for i in board.keys():
         if board [i] == "  ":
             return False
     return True
+
+def restartgame():
+    global winLabel
+    global fin_jeu
+    fin_jeu = False
+    for button in buttons:
+        button["text"] = "  "
+    for i in board.keys():
+        board[i] = "  "
+    titleLabel = Label(frame1,text="Tic Tac Toe",font=("Arial",25),bg="green",width=18)
+    titleLabel.grid(row=0, column=0,columnspan=3)
+    
+
 def play(event):
     global turn 
+    global fin_jeu
+    if fin_jeu:
+        return
     button = event.widget
     buttontext = str(button)
     clicked = buttontext[-1]
@@ -58,19 +77,24 @@ def play(event):
             button["text"]= "X"
             board[clicked] = turn
             if win(turn) :
-                winLabel = Label(frame2 , text=f"{turn} a gagne la partie",font=("Arial",20),bg="gray")
-                winLabel.grid(row= 3 , column= 0 , columnspan= 3)
+                winLabel = Label(frame1 , text=f"{turn} a gagne la partie",font=("Arial",25),bg="gray",width=18)
+                winLabel.grid(row= 0 , column= 0 , columnspan= 3)
+                fin_jeu = True
+        
             turn = "o"
         else:
             button["text"] = "O"
             board[clicked] = turn
             if win(turn) :
-                winLabel = Label(frame2 , text=f"{turn} a gagne la partie",font=("Arial",20), bg="gray")
-                winLabel.grid(row= 3 , column= 0 , columnspan= 3)
+                winLabel = Label(frame1 , text=f"{turn} a gagne la partie",font=("Arial",25), bg="gray",width=18)
+                winLabel.grid(row= 0 , column= 0 , columnspan= 3)
+                fin_jeu = True
             turn = "x"
-        if checkgame():
-            winLabel = Label(frame2 , text= "partie nulle" ,font=("Arial",20), bg="gray")
-            winLabel.grid(row= 3 , column= 0 , columnspan= 3)
+        if checkgamenulle():
+            nulleLabel = Label(frame1 , text= "partie nulle" ,font=("Arial",25), bg="gray",width=18)
+            nulleLabel.grid(row= 0 , column= 0 , columnspan= 3)
+            fin_jeu = True
+    
 #premiere range
 
 button1 = Button(frame2,text= "  ", width = 4 ,height = 2 , font=("Arial", 35), relief= RAISED )
@@ -113,4 +137,10 @@ button9 = Button(frame2,text= "  ", width = 4 ,height = 2 , font=("Arial", 35), 
 button9.grid(row=2,column=2)
 button9.bind("<Button-1>" , play)
 
+
+restarButton = Button(frame2 , text= "refaire une partie" , width=15 , height=1 , font=("Arial",20),bg="Green", 
+relief=RAISED , borderwidth=2,command=restartgame)
+restarButton.grid(row=4, column=0 , columnspan=3)
+
+buttons =[button1,button2,button3,button4,button5,button6,button7,button8,button9]
 root.mainloop()
